4082030e1a6dfed6d1a05bebca54a78a5f320f53
niccov <niccoventuri27@gmail.com>
2023-05-17 15:57:45 +0200

    stampa create comic

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -16,6 +16,27 @@
             </div>
 
             <div class="mb-3">
+                <label for="thumb">link copertina</label>
+                <input type="thumb" id="title" name="thumb">
+            </div>
+
+            <div class="mb-3">
+                <label for="price">prezzo</label>
+                <input type="text" id="price" name="price">
+            </div>
+
+            <div class="mb-3">
+                <label for="series">serie</label>
+                <input type="text" id="series" name="series">
+            </div>
+
+            <div class="mb-3">
+                <label for="sale_date">data di vendita</label>
+                <input type="text" id="sale_date" name="sale_date">
+            </div>
+
+
+            <div class="mb-3">
                 <label for="type">tipologia</label>
                 <input type="text" id="type" name="type">
             </div>
